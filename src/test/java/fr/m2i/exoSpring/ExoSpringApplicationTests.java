4ba9c46cf1f10a2f79a91271e2bfd905b0249411
Galais Valere <valeregbv@gmail.com>
2021-01-22 16:26:31 +0100

    ajout de models et controllers

---
+++ src/test/java/fr/m2i/exoSpring/ExoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.exoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
